b9b71a0468d686af7a9319a5ddc06faff4e6bbba
Pahang Krisdyan <pahangkrisdyan@gmail.com>
2025-06-16 19:01:57 +0700

    Fix JWT secret configuration for users-permissions plugin

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,7 @@
-export default () => ({});
+export default ({ env }) => ({
+  'users-permissions': {
+    config: {
+      jwtSecret: env('JWT_SECRET') || env('ADMIN_JWT_SECRET'),
+    },
+  },
+});
